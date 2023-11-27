4c3e7074b12ca1824c70e5356a005024be31f68c
PugazhendhiThangaprakasam <pugazhendhi.thangaprakasam@gmail.com>
2023-11-27 09:44:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./components/Footer";
+import Footer from "./components/Footer";
+import YourComponent from "./components/check";
+import Check from "./components/check";
+import DataFetch from "./components/dataFetch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Component from "./components/houseDetails";
+import Login from "./components/login";
+import Navbar from "./components/navbar";
+import Properties from "./components/properties";
+import SignUp from "./components/register";
+import SideNavBar from "./components/sideNavbar";
+import Slideshow from "./components/slide";
+
+
+function App()
+{
+    return(
+        <div>
+            {/* <Navbar/> */}
+            {/* <DataFetch/> */}
+            {/* <Check/> */}
+            {/* <Login/> */}
+            {/* <Properties/> */}
+            {/* <Component/> */}
+            {/* <Slideshow/> */}
+            <SideNavBar/>
+        </div>
+    )
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import router from './components/routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <RouterProvider router={router}>
+    
+  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
